d0627f9cffaeb9dcccfeb09e341485c6b54d925f
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-12 00:46:43 -0300

    iniciando projeto JPA

---
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -0,0 +1,10 @@
+package br.com.alura.loja.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")//ensinando o JPA a reconhecer o nome para buscar na tabela
+public class Produto {
+
+}
