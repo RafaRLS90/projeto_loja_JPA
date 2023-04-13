3cc154ca1c7dddd27d37972777590d9e2694e578
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-13 00:17:09 -0300

    Tabela produto pronta

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,13 +1,21 @@
 package br.com.alura.loja.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "produtos")//ensinando o JPA a reconhecer o nome para buscar na tabela
 public class Produto {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String descricao;
+	private BigDecimal preco;
 }
