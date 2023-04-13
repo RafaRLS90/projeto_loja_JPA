b17984537bcb8c032fdb7942b234d8171c584f04
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2023-04-13 00:00:12 -0300

    criando tabela produto

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -2,9 +2,12 @@ package br.com.alura.loja.modelo;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "produtos")//ensinando o JPA a reconhecer o nome para buscar na tabela
 public class Produto {
-
+	private Long id;
+	private String nome;
+	private String descricao;
 }
